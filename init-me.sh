43b182bc8fd506e4253a37b95de6376f415ba5ca
Cameron Boulton <cameronboulton@gmail.com>
2019-03-21 16:55:43 -0700

    Update init-me.sh

--- init-me.sh
+++ init-me.sh
@@ -7,5 +7,8 @@ xcode-select --install
 export CI=''
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install Homebrew
-brew install awscli bash-completion chezmoi gawk git gnu-sed go jq kops kubernetes-cli kubernetes-helm kubetail make node python redis ripgrep sceptre sipcalc watch wget yq
+# Install Homebrew formulae
+brew install awscli bash-completion chezmoi coreutils gawk git gnu-sed go jq kops kubernetes-cli kubernetes-helm kubetail make node python redis ripgrep sceptre sipcalc watch wget yq
+
+# Install Homebrew casks
+brew install spectacle visual-studio-code
