8237a6c597e9fba907127efd37ebb1df1df7844a
Cameron Boulton <cameronboulton@gmail.com>
2019-03-21 16:57:39 -0700

    Add Docker cask

--- init-me.sh
+++ init-me.sh
@@ -11,4 +11,4 @@ export CI=''
 brew install awscli bash-completion chezmoi coreutils gawk git gnu-sed go jq kops kubernetes-cli kubernetes-helm kubetail make node python redis ripgrep sceptre sipcalc watch wget yq
 
 # Install Homebrew casks
-brew cask install spectacle visual-studio-code
+brew cask install docker spectacle visual-studio-code
