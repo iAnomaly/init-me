fcb070023ebd584be2a20d38a1d88394fec936d3
Cameron Boulton <cameronboulton@gmail.com>
2019-03-21 17:18:55 -0700

    Add Google Chrome

--- init-macos.sh
+++ init-macos.sh
@@ -4,11 +4,11 @@
 xcode-select --install
 
 # Install Homebrew
-export CI=''
+export CI=
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install Homebrew formulae
 brew install awscli bash-completion chezmoi coreutils gawk git gnu-sed go jq kops kubernetes-cli kubernetes-helm kubetail make node python redis ripgrep sceptre sipcalc watch wget yq
 
 # Install Homebrew casks
-brew cask install docker spectacle visual-studio-code
+brew cask install docker google-chrome spectacle visual-studio-code
