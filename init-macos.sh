2d809b9625139fb6273c941223270f98483ddced
Cameron Boulton <cameronboulton@gmail.com>
2019-03-23 16:17:35 -0700

    Update init-macos.sh

--- init-macos.sh
+++ init-macos.sh
@@ -8,7 +8,10 @@ export CI=
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install Homebrew formulae
-brew install awscli bash-completion chezmoi coreutils gawk git gnu-sed go jq kops kubernetes-cli kubernetes-helm kubetail make node python redis ripgrep sceptre sipcalc watch wget yq
+brew install awscli bash-completion coreutils gawk git gnu-sed go jq kops kubernetes-cli kubernetes-helm kubetail make node python redis ripgrep sceptre sipcalc watch wget yq
 
 # Install Homebrew casks
 brew cask install docker google-chrome spectacle visual-studio-code
+
+# Install Homebrew taps
+brew install twpayne/taps/chezmoi
