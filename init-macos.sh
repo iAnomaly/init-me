87f04879e31b8a607750072dc8d762bf04aee6ab
Cameron Boulton <cameronboulton@gmail.com>
2019-05-09 13:04:36 -0700

    Update init-macos.sh

--- init-macos.sh
+++ init-macos.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Enable TouchID for PAM
+grep -q pam_tid.so /etc/pam.d/sudo || sudo sed -i '.bak' $'2i\\\nauth       sufficient     pam_tid\.so\n' /etc/pam.d/sudo
+
 # Install Xcode
 xcode-select --install
 
@@ -11,10 +14,10 @@ export CI=
 brew install awscli bash-completion coreutils gawk git gnu-sed go helmfile go jq kubernetes-cli kubernetes-helm make mysql@5.7 node python redis ripgrep sceptre sipcalc watch wget yq
 brew link --force mysql@5.7
 
+# Install Homebrew casks
+brew cask install docker google-chrome osxfuse spectacle visual-studio-code
+
 # Install Homebrew taps
 brew install datawire/blackbird/telepresence
 brew install twpayne/taps/chezmoi
 brew tap johanhaleby/kubetail && brew install kubetail
-
-# Install Homebrew casks
-brew cask install docker google-chrome osxfuse spectacle visual-studio-code
