9e922cfdbb55cf3fea21d836947fe07f5a100eed
Cameron Boulton <cameronboulton@gmail.com>
2019-05-09 11:37:01 -0700

    Update init-macos.sh

--- init-macos.sh
+++ init-macos.sh
@@ -8,11 +8,13 @@ export CI=
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install Homebrew formulae
-brew install awscli bash-completion coreutils gawk git gnu-sed go jq kops kubernetes-cli kubernetes-helm make node python redis ripgrep sceptre sipcalc watch wget yq
-
-# Install Homebrew casks
-brew cask install docker google-chrome spectacle visual-studio-code
+brew install awscli bash-completion coreutils gawk git gnu-sed go helmfile go jq kubernetes-cli kubernetes-helm make mysql@5.7 node python redis ripgrep sceptre sipcalc watch wget yq
+brew link --force mysql@5.7
 
 # Install Homebrew taps
+brew install datawire/blackbird/telepresence
 brew install twpayne/taps/chezmoi
 brew tap johanhaleby/kubetail && brew install kubetail
+
+# Install Homebrew casks
+brew cask install docker google-chrome osxfuse spectacle visual-studio-code
